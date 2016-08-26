3e64e3ee4027611de2658deb432cafdb9ef8483e
blatsha <blatsha@DelugeFrontServer.fios-router.home>
2016-08-26 12:30:38 -0400

    fixed playlists database table

---
+++ database/migrations/2016_08_26_162718_create_playlists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaylistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('playlist_id');
+            $table->string('name');
+            $table->string('view_on_spotify_href');
+            $table->string('spotify_uri')->nullable();
+            $table->integer('track_count')->nullable();
+            $table->string('user_id');
+            $table->timestamps();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('playlists');
+    }
+}
